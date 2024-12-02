1889ad647016797878ab1058aca5d9cfdbda07e0
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-12-02 12:22:20 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/js_110_ivndrkk@5e845c007dc45b4cdf31ea1c88cab954fa20d636 🚀

---
+++ index.js
@@ -0,0 +1,58 @@
+import{A as k,S as f,a as j,i as w}from"./assets/vendor-D1x2inJp.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))i(o);new MutationObserver(o=>{for(const t of o)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function s(o){const t={};return o.integrity&&(t.integrity=o.integrity),o.referrerPolicy&&(t.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?t.credentials="include":o.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(o){if(o.ep)return;o.ep=!0;const t=s(o);fetch(o.href,t)}})();document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector("#about-me"),n=document.querySelector("#projects"),s=document.querySelector("#work-together");document.querySelectorAll(".nav-link").forEach(o=>{o.addEventListener("click",t=>{t.preventDefault(),o.textContent.includes("About Me")?e.scrollIntoView({behavior:"smooth"}):o.textContent.includes("Projects")?n.scrollIntoView({behavior:"smooth"}):o.textContent.includes("Contacts")&&s.scrollIntoView({behavior:"smooth"})})})});const b=document.querySelector(".hero-button");b.addEventListener("click",function(e){e.preventDefault(),document.getElementById("work-together").scrollIntoView({behavior:"smooth"})});const x="/js_110_ivndrkk/assets/project-1-DDSEQAry.jpg",y="/js_110_ivndrkk/assets/project-12x-CXqOLe9D.jpg",E="/js_110_ivndrkk/assets/project-2-CnZ2isJx.jpg",L="/js_110_ivndrkk/assets/project-22x-yOt2G9ZS.jpg",S="/js_110_ivndrkk/assets/project-3-CBpo9lGd.jpg",I="/js_110_ivndrkk/assets/project-32x-DzBqeYZt.jpg",_="/js_110_ivndrkk/assets/project-4-8O6LE6YW.jpg",A="/js_110_ivndrkk/assets/project-42x-CM03vmRJ.jpg",C="/js_110_ivndrkk/assets/project-5-9d7B4pnA.jpg",B="/js_110_ivndrkk/assets/project-52x-D2rCJnjo.jpg",M="/js_110_ivndrkk/assets/project-6-C1hSzmeH.jpg",q="/js_110_ivndrkk/assets/project-62x-DCrUwTXX.jpg",J="/js_110_ivndrkk/assets/project-7-BGE4RsUG.jpg",P="/js_110_ivndrkk/assets/project-72x-DCVLQ1gE.jpg",T="/js_110_ivndrkk/assets/project-8-DOlwvTCj.jpg",D="/js_110_ivndrkk/assets/project-82x-D-BoNZSb.jpg",G="/js_110_ivndrkk/assets/project-9-C4ztqAgA.jpg",O="/js_110_ivndrkk/assets/project-92x-DJRCm-Uy.jpg",N="/js_110_ivndrkk/assets/project-10-7G_bG3ud.jpg",$="/js_110_ivndrkk/assets/project-102x-BrUkPh2C.jpg",R="/js_110_ivndrkk/assets/icons-BK_QZYKW.svg",a=[{title:"Wallet webservice",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":C,"2x":B}},{title:"Green harvest webservice",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":_,"2x":A}},{title:"English Exellence website",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":G,"2x":O}},{title:"Power pulse webservice ",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":x,"2x":y}},{title:"Mimino website",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":E,"2x":L}},{title:"Fruitbox online store",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":T,"2x":D}},{title:"Vyshyvanka vibes Landing Page",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":S,"2x":I}},{title:"Сhego jewelry website",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":M,"2x":q}},{title:"Energy flow webservice ",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":J,"2x":P}},{title:"starlight studio landing page",technologies:"React, JavaScript, Node JS, Git",link:"https://ivndrkk.github.io/portfolio2.0/",images:{"1x":N,"2x":$}}];let c=0;const V=3,H=document.getElementById("myProjectsList"),u=document.getElementById("load-more-btn");function F(e){return`
+    <li class="my-projects-item">
+      <div class="image-container">
+        <picture>
+        <source
+          srcset="
+            ${e.images["1x"]} 1x,
+            ${e.images["2x"]} 2x
+          "
+          media="(min-width: 320px)"
+        />
+        <img
+          class="my-projects-img"
+          src="${e.images["1x"]}"
+          alt="${e.title}"
+        />
+      </picture>
+       </div>
+      <p class="my-projects-technologies">${e.technologies}</p>
+      <div class="my-projects-card-row">
+        <h3 class="my-projects-names">${e.title}</h3>
+        <a class="my-projects-link" href="${e.link}" target="_blank">
+          Visit
+          <svg class="icon-arrow" width="24" height="24">
+            <use href="${R}#icon-up-right-arrow"></use>
+          </svg>
+        </a>
+      </div>
+    </li>
+  `}function p(){const e=a.length-c,n=Math.min(V,e);for(let s=0;s<n;s++){const i=F(a[c]);H.insertAdjacentHTML("beforeend",i),c++}c>=a.length&&(u.style.display="none")}u.addEventListener("click",p);p();document.addEventListener("DOMContentLoaded",()=>{new k(".accordion-container",{duration:400,showMultiple:!1,openOnInit:[0]})});document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector(".reviews-list");function n(s){return s.map(({author:i,avatar_url:o,review:t})=>`
+        <li class="swiper-slide" id="reviews-list-item">
+     <p class="reviews-item-text">${t}</p>
+     <div class="img-title-wraper">
+            <img class="reviews-item-img"
+                 src=${o}
+                 alt="image of ${i}"
+                 width="40"
+                 height="40">
+            <h3 class="reviews-item-title">${i}</h3>
+          </div>          
+        </li>`).join("")}fetch("https://portfolio-js.b.goit.study/api/reviews").then(s=>{if(!s.ok)throw new Error("Not found");return s.json()}).then(s=>{e.insertAdjacentHTML("beforeend",n(s));const i=new f(".swiper-container",{slidesPerView:1,spaceBetween:32,navigation:{nextEl:"#swiper-button-next",prevEl:"#swiper-button-prev"},breakpoints:{1280:{slidesPerView:2,spaceBetween:32,slidesPerGroup:2}},simulateTouch:!0,autoHeight:!1,on:{init:function(){const r=document.querySelector("#swiper-button-prev");this.isBeginning&&r.classList.add("disabled")},reachBeginning:function(){document.querySelector("#swiper-button-prev").classList.add("disabled")},fromEdge:function(){const r=document.querySelector("#swiper-button-prev"),l=document.querySelector("#swiper-button-next");r.classList.remove("disabled"),l.classList.remove("disabled")},reachEnd:function(){document.querySelector("#swiper-button-next").classList.add("disabled")}}}),o=document.querySelector("#swiper-button-next"),t=document.querySelector("#swiper-button-prev");o.addEventListener("click",()=>{i.slideNext()}),t.addEventListener("click",()=>{i.slidePrev()})}).catch(s=>{e.insertAdjacentHTML("beforeend",`<li>
+          <p>${s.message}</p>
+        </li>`)})});document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("contactForm"),n=document.querySelector(".wt-popup"),s="https://portfolio-js.b.goit.study/api/requests",i=t=>{if(t){const r=window.innerWidth-document.documentElement.clientWidth;document.documentElement.style.setProperty("--scrollbar-width",`${r}px`),document.body.classList.add("wt-popup-open"),n.style.display="flex"}else document.body.classList.remove("wt-popup-open"),n.style.display="none",document.documentElement.style.setProperty("--scrollbar-width","0px")};document.querySelector(".wt-close-btn").addEventListener("click",()=>{i(!1)}),window.addEventListener("click",t=>{t.target===n&&i(!1)}),window.addEventListener("keydown",t=>{t.key==="Escape"&&i(!1)}),e.addEventListener("submit",async t=>{t.preventDefault();const r=document.getElementById("email").value,l=document.getElementById("message").value,v={email:r,comment:l};try{if((await j.post(s,v,{headers:{"Content-Type":"application/json",Accept:"application/json"}})).status===201)i(!0),e.reset();else throw new Error("Unexpected response status")}catch(d){console.error("Error submitting form:",d),w.error({title:"Error",message:"Something went wrong. Please try again later.",backgroundColor:"#E74A3B",close:!1,closeOnClick:!0})}});const o=document.getElementById("email");o.addEventListener("input",()=>{const t=o.nextElementSibling.nextElementSibling;o.validity.valid?t.style.display="none":t.style.display="block"})});document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector(".burger"),n=document.querySelector(".menu-icon-close"),s=document.querySelector(".mob-menu"),i=document.querySelectorAll(".mob-nav-link");function o(){s.classList.add("is-open"),e.hidden=!0,n.hidden=!1}function t(){s.classList.remove("is-open"),e.hidden=!1,n.hidden=!0}e==null||e.addEventListener("click",o),n==null||n.addEventListener("click",t),i.forEach(r=>{r.addEventListener("click",t)})});function U(){return`
+    <svg width="48" height="24" viewBox="0 0 48 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="48" height="24" rx="12" fill="#E4E5E6"/>
+      <circle cx="12" cy="12" r="10" fill="#00B068"/>
+    </svg>
+  `}function W(){return`
+    <svg width="48" height="24" viewBox="0 0 48 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="48" height="24" rx="12" fill="#292929"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M32 21.1679C35.5318 19.6248 38 16.1006 38 12C38 7.89936 35.5318 4.3752 32 2.83209C33.2249 2.29689 34.5778 2 36 2C41.5228 2 46 6.47715 46 12C46 17.5228 41.5228 22 36 22C34.5778 22 33.2249 21.7031 32 21.1679Z" fill="#00B068"/>
+    </svg>
+  `}function m(){const e=document.documentElement.getAttribute("data-theme"),n=document.getElementById("themeIcon");n.innerHTML=e==="light"?U():W()}function Z(){const e=document.documentElement;e.getAttribute("data-theme")==="light"?(e.setAttribute("data-theme","dark"),localStorage.setItem("theme","dark")):(e.setAttribute("data-theme","light"),localStorage.setItem("theme","light")),m()}document.addEventListener("DOMContentLoaded",()=>{const e=localStorage.getItem("theme"),n=document.documentElement;e?n.setAttribute("data-theme",e):n.setAttribute("data-theme","light"),m()});document.querySelector("#themeToggleButton").addEventListener("click",Z);const z=document.querySelectorAll(".ac-trigger");function g(e){z.forEach(n=>{const s=n.querySelector(".icon-1"),i=n.querySelector(".icon-2");s&&s.remove(),i&&i.remove();const o=e==="dark"?"#F0F0F0":"#292929",t=document.createElementNS("http://www.w3.org/2000/svg","svg");t.setAttribute("class","icon-1"),t.setAttribute("width","24"),t.setAttribute("height","24"),t.setAttribute("viewBox","0 0 24 24"),t.setAttribute("fill","none"),t.innerHTML=`
+      <path d="M18 9L12 15L6 9" stroke="${o}" stroke-width="2" stroke-linecap="round" />
+    `;const r=document.createElementNS("http://www.w3.org/2000/svg","svg");r.setAttribute("class","icon-2"),r.setAttribute("width","24"),r.setAttribute("height","24"),r.setAttribute("viewBox","0 0 24 24"),r.setAttribute("fill","none"),r.innerHTML=`
+      <path d="M18 15L12 9L6 15" stroke="${o}" stroke-width="2" stroke-linecap="round" />
+    `,n.appendChild(t),n.appendChild(r)})}function h(){return document.documentElement.getAttribute("data-theme")||(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light")}const K=new MutationObserver(()=>{const e=h();g(e)});K.observe(document.documentElement,{attributes:!0,attributeFilter:["data-theme"]});g(h());
+//# sourceMappingURL=index.js.map
